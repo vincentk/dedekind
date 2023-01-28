36b67f3fd596850dcccbe7102c9577fb878f4ccc
Vincent Kraeutler <vincent.kraeutler@axpo.com>
2023-01-28 22:16:01 +0100

    .

--- dedekind-matrices/src/test/java/com/github/vincentk/dedekind/linear/primitives/DoublesTest.java
+++ dedekind-matrices/src/test/java/com/github/vincentk/dedekind/linear/primitives/DoublesTest.java
@@ -12,26 +12,28 @@ import com.github.vincentk.dedekind.linear.Vector;
 
 public class DoublesTest {
 
-	@Test
-	public void testConstruction() {
-		assertThrows(AssertionError.class, () -> doubles(null));
-		
-		final Doubles subject = doubles(new double[0]);
-		assertNotNull(subject);
-		
-		assertTrue(subject.equals(subject));
-	}
-	
-	@Test
-	public void testVectorOps() {
-		
-		final Vector<Rs, Doubles> subject = doubles(new double[] {1, 2});
-		
-		// Monoid identity operations:
-		assertThat(subject.mult(Rs.ONE)).isEqualTo(subject);
-		
-		final var zeros = doubles(new double[] {0, 0});
-		
-		assertThat(subject.plus(zeros)).isEqualTo(subject);
-	}
+    @Test
+    public void testConstruction() {
+        assertThrows(AssertionError.class, () -> doubles(null));
+
+        assertThrows(AssertionError.class, () -> doubles(new double[0]));
+
+        final var subject = doubles(new double[] {0});
+        assertNotNull(subject);
+
+        assertTrue(subject.equals(subject));
+    }
+
+    @Test
+    public void testVectorOps() {
+
+        final Vector<Rs, Doubles> subject = doubles(new double[] {1, 2});
+
+        // Monoid identity operations:
+        assertThat(subject.mult(Rs.ONE)).isEqualTo(subject);
+
+        final var zeros = doubles(new double[] {0, 0});
+
+        assertThat(subject.plus(zeros)).isEqualTo(subject);
+    }
 }
