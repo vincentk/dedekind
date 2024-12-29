2e327de585d110cf799bb44f0a98d01e48f51e11
vincentk <vincent.kraeutler@gmail.com>
2024-12-29 17:17:54 +0100

    .

--- dedekind-ontology/src/main/java/com/github/vincentk/dedekind/sets/Set.java
+++ dedekind-ontology/src/main/java/com/github/vincentk/dedekind/sets/Set.java
@@ -1,5 +1,7 @@
 package com.github.vincentk.dedekind.sets;
 
+import java.util.function.Predicate;
+
 import com.github.vincentk.dedekind.sets.binary.relation.homogeneous.Identity;
 
 /**
@@ -30,6 +32,36 @@ Identity<T>
      * @return true exactly if this is &empty;.
      */
     boolean isEmpty();
+    
+    /**
+     * @param that
+     * @return this &cap; that
+     */
+    Set<E, ?> intersection(Set<E, ?> that);
+    
+    /**
+     * @param that
+     * @return this &cup; that
+     */
+    Set<E, ?> union(Set<E, ?> that);
+    
+    /**
+     * @param that
+     * @return {x &isin; this | Φ(x)}
+     */
+    Set<E, ?> where(Predicate<E> Φ);
+    
+    /**
+     * @param that
+     * @return this &sub; that
+     */
+    boolean sub(Set<E, ?> that);
+    
+    /**
+     * @param that
+     * @return this &sup; that
+     */
+    boolean sup(Set<E, ?> that);
 
     /**
      * An element of a set.
