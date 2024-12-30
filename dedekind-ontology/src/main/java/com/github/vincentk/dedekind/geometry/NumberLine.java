e3bcb871d47f9c6290d9cbf0acf9fb6d7ee310e9
vincentk <vincent.kraeutler@gmail.com>
2024-12-30 18:46:28 +0100

    Adding ConnectedSpace.

--- dedekind-ontology/src/main/java/com/github/vincentk/dedekind/geometry/NumberLine.java
+++ dedekind-ontology/src/main/java/com/github/vincentk/dedekind/geometry/NumberLine.java
@@ -6,12 +6,13 @@ import com.github.vincentk.dedekind.sets.Cardinality;
 import com.github.vincentk.dedekind.sets.ordered.TotallyOrdered;
 
 /**
- * A specialization of the {@link MetricSpace} to sets which are {@link TotallyOrdered}.
+ * A specialization of a {@link MetricSpace} to sets which are {@link TotallyOrdered}.
  * 
  * @param <C>
  * @param <T>
  * 
  * @see https://en.wikipedia.org/wiki/Number_line
+ * @see https://en.wikipedia.org/wiki/Interval_(mathematics)#Properties
  */
 public interface NumberLine<
 E extends MetricSpace.Me<E, E> & TotallyOrdered.Oe<E> & SemiRing.SmrE<E>,
