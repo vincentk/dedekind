b7167b1dc4e883682627758892fb1cb464690ad1
vincentk <vincent.kraeutler@gmail.com>
2026-04-10 14:29:59 +0200

    .

--- src/test/cpp/modules/dedekind/category/limit_test.cpp
+++ src/test/cpp/modules/dedekind/category/limit_test.cpp
@@ -13,8 +13,6 @@ TEST_CASE("Discrete: Terminal Object (1) - Pure Existence",
   SECTION("Existence Axiom") {
     STATIC_CHECK(IsTerminalObject<One>);
 
-    CHECK(One{} == One{});
-
     // Every species T must have exactly one morphism to the Terminal Object (T
     // -> 1)
     STATIC_CHECK(HasUniqueMorphismTo<int, One>);
