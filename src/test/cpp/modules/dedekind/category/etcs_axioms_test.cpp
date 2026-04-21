efa49dee5bd5c116e44f1df084417ac5e7f475e0
vincentk <vincent.kraeutler@gmail.com>
2026-04-21 10:28:06 +0200

    fix: align SplitEpiLawFixture constraints with law helper

--- src/test/cpp/modules/dedekind/category/etcs_axioms_test.cpp
+++ src/test/cpp/modules/dedekind/category/etcs_axioms_test.cpp
@@ -327,7 +327,8 @@ TEST_CASE(
  * criteria. Construct with a concrete epi/section pair and call the helpers.
  */
 template <typename Epi, typename Section>
-  requires IsSplitEpicPair<Epi, Section>
+  requires IsSplitEpicPair<Epi, Section> &&
+           std::equality_comparable<Cod<Epi>>
 struct SplitEpiLawFixture {
   Epi epi;
   Section section;
